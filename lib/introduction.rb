ba17c811cf623450ed80100c408312c03fbf65e2
helpful-founder-6136 <mariam.tamimi@gmail.com>
2018-11-25 23:47:22 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,5 +3,5 @@ def introduction(name)
 end
 
 def introduction_with_language(name,language)
-  puts "Hi #{name}, you are a good #{language} programmer!"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
